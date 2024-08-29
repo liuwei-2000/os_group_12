48ac9a2fecae9181aa9ca629206d5cf53a687ce3
liuwe <liuwe@chalmers.se>
2024-08-29 23:42:11 +0200

    Add split command function

--- code/exe_functions.c
+++ code/exe_functions.c
@@ -37,3 +37,45 @@ void execute_date_function() {
         exit(EXIT_FAILURE);
     }
 }
+/*
+    The split Command function took in string, and a command number counter
+    Further use in calling the function
+    char input[] = "ls | pwd | date";
+    int command_count = 0;
+
+    char** commands = splitString(input, &command_count);
+
+    for (int i = 0; i < command_count; i++) {
+        -- execute commands [i];
+        free(commands[i]);
+    }
+ */
+char** splitCommand(char* str, int* count) {
+    int commands_count = 0;
+    char* tempStr = strdup(str); // Make a copy of the string for manipulation
+    char* token = strtok(tempStr, "|");
+    
+    // Count the number of commands
+    while (token != NULL) {
+        commands_count++;
+        token = strtok(NULL, "|");
+    }
+
+    // Allocate memory for the commands array
+    char** commands = malloc(commands_count * sizeof(char*));
+    strcpy(tempStr, str); // Reset the string copy
+    token = strtok(tempStr, "|");
+
+    // Extract commands
+    for (int i = 0; i < commands_count; i++) {
+        commands[i] = strdup(token); // Duplicate the token to the array
+        token = strtok(NULL, "|");
+    }
+
+    // Clean up
+    free(tempStr);
+
+    *count = commands_count;
+    return commands;
+
+}
--- code/exe_functions.h
+++ code/exe_functions.h
@@ -2,3 +2,4 @@ void execute_ls_function();
 void execute_who_function();
 void execute_date_function();
 void execute_pwd_function();
+char** splitCommand(char*, int*);
